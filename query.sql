a37d14141990ff5c586458702e701bef161bc6ca
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2023-01-13 16:01:08 -0500

    Ejercicio 7 Resuelto - Query

--- query.sql
+++ query.sql
@@ -20,11 +20,12 @@ SELECT CONCAT("b_lname", ' ', "b_fname") AS "Full_name" FROM
   (builder INNER JOIN house ON builder.bid = house.bid)
 WHERE (h_name = 'Nova' OR h_name = 'El cañaduzal');
 
-
-
 --Ejercicio 7.
-
+SELECT (h_name) FROM 
+  (builder INNER JOIN house ON builder.bid = house.bid) 
+WHERE (b_lname LIKE 'P%');
 
 --Ejercicio 8.
 SELECT (n_name,city) FROM 
-  (SELECT * FROM(house INNER JOIN neighborhood ON house.nid = neighborhood.nid) WHERE (h_name = 'Las consultas')) AS "datos_completos";
+  (SELECT * FROM(house INNER JOIN neighborhood ON house.nid = neighborhood.nid) WHERE (h_name = 'Las consultas')) 
+AS "datos_completos";
