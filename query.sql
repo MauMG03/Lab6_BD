d7a02a43da978fee9aa47073b9339a5db96d268e
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2023-01-18 08:33:43 -0500

    Modificaciones a consultas

--- query.sql
+++ query.sql
@@ -1,11 +1,10 @@
 --Ejercicio 1.
-select concat(s_Fname, ' ', s_lname) as "Nombre completo"
-from subcontractor
-where salary > 3000000
-group by s_fname, s_lname 
+SELECT concat(s_Fname, ' ', s_lname) AS "Nombre completo"
+FROM subcontractor
+WHERE salary > 3000000
 
 --Ejercicio 2.
-select s_lname, s_Fname 
+select s_Fname, b_Fname 
 from subcontractor natural join hireinfo natural join builder
 where extract(year from hiredate)::int = 2019;
 
@@ -19,7 +18,7 @@ select b_lname, s_lname, salary
 from subcontractor natural join hireinfo natural join builder
 
 --Ejercicio 5.
-SELECT (b_lname, h_name) FROM 
+SELECT b_lname, h_name FROM 
   (builder LEFT OUTER JOIN house ON builder.bid = house.bid) 
 WHERE (finishdate > CURRENT_DATE OR h_name IS NULL) 
 ORDER BY b_lname;
@@ -30,12 +29,15 @@ SELECT CONCAT("b_lname", ' ', "b_fname") AS "Full_name" FROM
 WHERE (h_name = 'Nova' OR h_name = 'El cañaduzal');
 
 --Ejercicio 7.
-SELECT (h_name) FROM 
+SELECT b_Fname, b_lname, h_name FROM 
   (builder INNER JOIN house ON builder.bid = house.bid) 
 WHERE (b_lname LIKE 'P%');
 
 --Ejercicio 8.
-SELECT (n_name,city) FROM 
-  (house INNER JOIN neighborhood ON house.nid = neighborhood.nid) 
+--Verificar que exista una casa llamada 'Las consultas'
+SELECT EXISTS(SELECT h_name FROM house WHERE h_name = 'Las consultas');
+
+SELECT n_name,city FROM 
+	(house INNER JOIN neighborhood ON house.nid = neighborhood.nid) 
 WHERE (h_name = 'Las consultas');
 
